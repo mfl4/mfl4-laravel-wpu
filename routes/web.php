7e5a265e9245e8917193a0eeeced1a0a1cf23e8b
mfl4 <mhdfarhanl6267@gmail.com>
2023-10-12 03:28:02 +0700

    feat: 4. Blade Templating Engine has been learnt

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home",
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Mhd. Farhan Lubis",
         "email" => "farhan@gmail.com",
         "image" => "farhan.jpg",
@@ -26,5 +29,52 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Antologi Senandika",
+            "slug" => "antologi-senandika",
+            "author" => "Freya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ipsa magni repellat fuga autem nam doloremque delectus nemo dolor praesentium possimus, unde non, assumenda rem quam, inventore reiciendis ipsum laborum id placeat blanditiis neque saepe. Inventore aut obcaecati reprehenderit debitis corporis rem id quis, dolorum exercitationem vel accusantium ducimus corrupti atque voluptatibus cum sit voluptatem consequatur molestias. Laborum iure, labore, soluta libero dolor nulla, assumenda non magnam aliquid in at repudiandae? Ullam est quaerat inventore, dolore iste tenetur quasi possimus id neque deserunt, sit ratione saepe repellat quod praesentium nihil fugiat. Perferendis corrupti quos quisquam unde cumque voluptate iusto iste!."
+        ],
+        [
+            "title" => "Lihatlah",
+            "slug" => "lihatlah",
+            "author" => "Nunung",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ipsa magni repellat fuga autem nam doloremque delectus nemo dolor praesentium possimus, unde non, assumenda rem quam, inventore reiciendis ipsum laborum id placeat blanditiis neque saepe. Inventore aut obcaecati reprehenderit debitis corporis rem id quis, dolorum exercitationem vel accusantium ducimus corrupti atque voluptatibus cum sit voluptatem consequatur molestias. Laborum iure, labore, soluta libero dolor nulla, assumenda non magnam aliquid in at repudiandae? Ullam est quaerat inventore, dolore iste tenetur quasi possimus id neque deserunt, sit ratione saepe repellat quod praesentium nihil fugiat. Perferendis corrupti quos quisquam unde cumque voluptate iusto iste!."
+        ]
+    ];
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Antologi Senandika",
+            "slug" => "antologi-senandika",
+            "author" => "Freya",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ipsa magni repellat fuga autem nam doloremque delectus nemo dolor praesentium possimus, unde non, assumenda rem quam, inventore reiciendis ipsum laborum id placeat blanditiis neque saepe. Inventore aut obcaecati reprehenderit debitis corporis rem id quis, dolorum exercitationem vel accusantium ducimus corrupti atque voluptatibus cum sit voluptatem consequatur molestias. Laborum iure, labore, soluta libero dolor nulla, assumenda non magnam aliquid in at repudiandae? Ullam est quaerat inventore, dolore iste tenetur quasi possimus id neque deserunt, sit ratione saepe repellat quod praesentium nihil fugiat. Perferendis corrupti quos quisquam unde cumque voluptate iusto iste!."
+        ],
+        [
+            "title" => "Lihatlah",
+            "slug" => "lihatlah",
+            "author" => "Nunung",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ipsa magni repellat fuga autem nam doloremque delectus nemo dolor praesentium possimus, unde non, assumenda rem quam, inventore reiciendis ipsum laborum id placeat blanditiis neque saepe. Inventore aut obcaecati reprehenderit debitis corporis rem id quis, dolorum exercitationem vel accusantium ducimus corrupti atque voluptatibus cum sit voluptatem consequatur molestias. Laborum iure, labore, soluta libero dolor nulla, assumenda non magnam aliquid in at repudiandae? Ullam est quaerat inventore, dolore iste tenetur quasi possimus id neque deserunt, sit ratione saepe repellat quod praesentium nihil fugiat. Perferendis corrupti quos quisquam unde cumque voluptate iusto iste!."
+        ]
+    ];
+
+    $newPost = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $newPost = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $newPost
+    ]);
 });
