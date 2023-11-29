5fcc78f33d590b79b437ecaa0b08535fef35a463
mfl4 <mhdfarhanl6267@gmail.com>
2023-11-29 17:51:35 +0700

    feat: 8. Post Category & Eloquent Relationship

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">All Posts</h1>
+    <h1 class="mb-5">Posts Category : {{ $category }}</h1>
 
     @foreach ($posts as $post)
         <article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-    <article class="mb-5">
-        <h2>{{ $post->title }}</h2>
-        {!! $post->body !!}
-    </article>
+    <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By Mhd. Farhan Lubis in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+    {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
 @endsection
